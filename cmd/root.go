92eba25789979897d0035e8ef08a323ea4e48d38
Vidal Vasconcelos <vasconcelosvidal@gmail.com>
2020-08-20 20:52:54 -0300

    feat: remove useless error print

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -19,7 +18,6 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
