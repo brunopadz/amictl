ffac2fe1eab42145fb03312729c61f175c26a60a
brunopadz <bpadz@protonmail.com>
2020-10-05 20:16:17 -0300

    feat: display different messages when not using --cost flag

--- providers/aws/render.go
+++ providers/aws/render.go
@@ -13,6 +13,7 @@ func Render(cmd *cobra.Command, region string, describeImagesOutput *ec2.Describ
 	red := color.New(color.FgRed)
 	yellow := color.New(color.FgYellow)
 	green := color.New(color.FgGreen)
+	title := color.New(color.Bold)
 
 	okmsg := "\nEverything looks good."
 	warnmsg := "\nYou could be saving some money."
@@ -24,28 +25,28 @@ func Render(cmd *cobra.Command, region string, describeImagesOutput *ec2.Describ
 		return err
 	}
 
-	for _, ami := range describeImagesOutput.Images {
-		cmd.Printf("%s", aws.StringValue(ami.ImageId))
-		if cost {
+	if cost {
+		title.Println("AMI ID\t\t\tSize\tMonthly Cost")
+		for _, ami := range describeImagesOutput.Images {
 			var volumeSize = aws.Int64Value(ami.BlockDeviceMappings[0].Ebs.VolumeSize)
 			volume += volumeSize
 
-			cmd.Printf("\t size: %d GB ", volumeSize)
-			cmd.Printf("\t monthly cost: U$ %g", GetAmiPriceBySize(volumeSize, region))
+			cmd.Printf("%s", aws.StringValue(ami.ImageId))
+			cmd.Printf("\t%d GB ", volumeSize)
+			cmd.Printf("\tUS$ %g\n", GetAmiPriceBySize(volumeSize, region))
 		}
-		cmd.Println()
-	}
-
-	if len(describeImagesOutput.Images) == 0 {
-		green.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", okmsg, getcost)
-	} else if len(describeImagesOutput.Images) >= 1 && len(describeImagesOutput.Images) <= 10 {
-		yellow.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", warnmsg, getcost)
+		cmd.Println("Estimated cost monthly: US$", GetAmiPriceBySize(volume, region))
 	} else {
-		red.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", alertmsg, getcost)
-	}
-
-	if cost {
-		cmd.Printf("Estimated cost monthly: U$ %g \n", GetAmiPriceBySize(volume, region))
+		for _, ami := range describeImagesOutput.Images {
+			cmd.Println(aws.StringValue(ami.ImageId))
+		}
+		if len(describeImagesOutput.Images) == 0 {
+			green.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", okmsg, getcost)
+		} else if len(describeImagesOutput.Images) >= 1 && len(describeImagesOutput.Images) <= 10 {
+			yellow.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", warnmsg, getcost)
+		} else {
+			red.Println("Total of ", len(describeImagesOutput.Images), "AMIs.", alertmsg, getcost)
+		}
 	}
 
 	return nil
